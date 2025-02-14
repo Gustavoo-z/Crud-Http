737dcb0f963fd5349bd0c82fd9165ebe49b6a85f
Gustavo Zesczylinski <germangz@rocketmail.com>
2025-02-14 10:49:54 -0300

    Add put

--- scripts/api.js
+++ scripts/api.js
@@ -14,7 +14,7 @@ const api = {
             const response = await fetch('http://localhost:3000/pensamentos', {
                 method: "POST",
                 headers: {
-                    "Content-Type": "aplication/json"
+                    "Content-Type": "application/json"
                 },
                 body: JSON.stringify(pensamento)
             })
--- scripts/main.js
+++ scripts/main.js
@@ -1,20 +1,11 @@
 import ui from "./ui.js"
 import api from "./api.js"
 
-document.addEventListener('DOMContentLoaded', () => {
-    ui.renderizarPensamentos()
-
-    const formulario = document.getElementById('pensamento-form')
-    formulario.addEventListener('submit', manipularSubmit)
-})
-
 async function manipularSubmit(event) {
-    event.preventDefault(); //Resolver preventDefault
+    event.preventDefault();
     const id = document.getElementById('pensamento-id').value;
     const conteudo = document.getElementById('pensamento-conteudo').value;
     const autoria = document.getElementById('pensamento-autoria').value;
-
-    console.log("Conteúdo:", conteudo, "Autoria:", autoria);
 
     try {
         await api.salvarPensamento({conteudo, autoria})
@@ -23,3 +14,16 @@ async function manipularSubmit(event) {
         alert('Erro ao salvar pensamento.')
     }
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+    ui.renderizarPensamentos()
+
+    const formulario = document.getElementById('pensamento-form')
+    formulario.addEventListener('submit', manipularSubmit)
+})
+
+const botaoCancelar = document.querySelector('#botao-cancelar');
+
+botaoCancelar.addEventListener('click', () => {
+    document.getElementById("pensamento-form").reset();
+})
